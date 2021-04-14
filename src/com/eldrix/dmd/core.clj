45391fb6509e3019dacef4f01689614b3c2f9876
Mark Wardle <mark@wardle.org>
2021-04-14 07:23:17 +0100

    Automatic bootstrapping.
    Preparation for command-line tooling; wow downloads latest dm+d distribution and creates new file-based database in one call.

--- src/com/eldrix/dmd/core.clj
+++ src/com/eldrix/dmd/core.clj
@@ -1,16 +1,37 @@
 (ns com.eldrix.dmd.core
   (:require [clojure.core.async :as a]
+            [clojure.tools.logging.readable :as log]
             [com.eldrix.dmd.import :as dim]
-            [com.eldrix.dmd.store :as st]))
+            [com.eldrix.dmd.store :as st]
+            [clojure.string :as str]
+            [com.eldrix.trud.zip :as zipfile])
+  (:import (java.time.format DateTimeFormatter)))
 
 (defn import-dmd
+  "Creates a new dm+d filestore at `filename` from the directory specified."
   [filename dir]
   (let [ch (a/chan)]
     (a/thread (dim/stream-dmd dir ch :ordered? true))
     (st/create-store filename ch)))
 
+(defn install-latest
+  "Create a versioned dm+d file-based database by downloading the latest dm+d
+  distribution automatically from NHS Digital's TRUD service."
+  [api-key-file cache-dir]
+  (let [api-key (str/trim-newline (slurp api-key-file))
+        release (com.eldrix.dmd.download/download-release api-key cache-dir)
+        _ (log/info "Downloaded dm+d release " release)
+        unzipped (zipfile/unzip (:archiveFilePath release))
+        filename (str "dmd-" (.format (DateTimeFormatter/ISO_LOCAL_DATE) (:releaseDate release )) ".db")]
+    (log/info "Creating dm+d file-based database :" filename)
+    (import-dmd filename (.toFile unzipped))
+    (zipfile/delete-paths [unzipped])))
+
 (comment
   (import-dmd "dmd.db" "/Users/mark/Downloads/nhsbsa_dmd_3.4.0_20210329000001")
+  (install-latest "/Users/mark/Dev/trud/api-key.txt" "/var/tmp/trud")
+
+
   (def store (st/open-dmd-store "dmd.db"))
   (.close store)
   (def vtm (st/fetch-product store 108537001))
---
+++ src/com/eldrix/dmd/download.clj
@@ -0,0 +1,53 @@
+(ns com.eldrix.dmd.download
+  (:require [clojure.string :as str]
+            [com.eldrix.trud.core :as trud]
+            [com.eldrix.trud.zip :as zipfile]
+            [com.eldrix.dmd.import :as dim])
+  (:import (java.time.format DateTimeFormatter)
+           (java.time LocalDate)))
+
+
+(def dm+d-trud-identifier 24)
+
+
+(defn fetch-available-releases
+  "Return a list of the available releases."
+  [api-key]
+  (trud/get-releases api-key dm+d-trud-identifier))
+
+(defn print-available-releases
+  [api-key]
+  (clojure.pprint/print-table [:id :releaseDate :name] (fetch-available-releases api-key)))
+
+(defn download-release
+  "Download a dm+d distribution from TRUD
+  Parameters:
+  - api-key      : api key
+  - cache-dir    : TRUD download cache directory (e.g. \"/var/trud/cache/\".
+  - release-date : optional, release date of distribution (default, latest).
+                 : can be a string or a java.time.LocalDate for convenience.
+  Returns TRUD metadata about the item specified, but also the path to the
+  downloaded archive file.
+  See com.eldrix.trud.core/get-latest for information about return value."
+  ([api-key cache-dir] (download-release api-key cache-dir nil))
+  ([api-key cache-dir release-date]
+  (if-let [release-date' (cond (instance? LocalDate release-date) release-date
+                               (string? release-date) (LocalDate/parse release-date DateTimeFormatter/ISO_DATE))]
+    ;; if we have an explicit release date, find it in the releases available, and then download it.
+    (let [available (fetch-available-releases api-key)]
+      (if-let [release (first (filter #(= release-date' (:releaseDate %)) available))] ;; find the release based on the release date specified
+        (assoc release :archiveFilePath (trud/download-release cache-dir release)) ;; and once found, download it.
+        (throw (ex-info "No release found for date" {:release-date release-date' :available (map :releaseDate available)}))))
+    ;; if we don't have an explicit release date, get the latest
+    (trud/get-latest {:api-key   api-key
+                      :cache-dir cache-dir}
+                     dm+d-trud-identifier))))
+
+(comment
+  (def api-key (str/trim-newline (slurp "/Users/mark/Dev/trud/api-key.txt")))
+  api-key
+
+  (print-available-releases api-key)
+  (def release (download-release api-key "/var/tmp/trud"))
+
+  )
