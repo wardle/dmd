3e360585c826ec608cb57164302d2a14070f88f4
Mark Wardle <mark@wardle.org>
2021-09-05 12:41:03 +0100

    Add lookups test

--- test/src/com/eldrix/dmd/importer_test.clj
+++ test/src/com/eldrix/dmd/importer_test.clj
@@ -6,5 +6,16 @@
 (deftest file-ordering
   (is (= '(:LOOKUP :INGREDIENT :VTM :VMP :AMP :VMPP :AMPP) (map :type (dim/dmd-file-seq (io/resource "dmd-empty"))))))
 
+(deftest import-lookups
+  (is (= {:TYPE [:LOOKUP :BASIS_OF_NAME],
+          :CD "0001",
+          :DESC "rINN - Recommended International Non-proprietary",
+          :ID :BASIS_OF_NAME-0001}
+         (first (dim/get-component (io/resource "dmd-empty") :LOOKUP :BASIS_OF_NAME)))))
+
+(deftest metadata
+  (is (= (java.time.LocalDate/of 2021 8 26) (:release-date (dim/get-release-metadata (io/resource "dmd-empty"))))))
+
+
 (comment
   (run-tests))
