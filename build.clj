b3078767167442c00c53e6bc9b1a1a05dfa4113f
Mark Wardle <mark@wardle.org>
2024-02-20 15:00:12 +0000

    Prepare for v1.0.174

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'com.eldrix/dmd)
-(def version (format "1.0.%s-alpha" (b/git-count-revs nil)))
+(def version (format "1.0.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def jar-basis (b/create-basis {:project "deps.edn"}))
 (def uber-basis (b/create-basis {:project "deps.edn"
